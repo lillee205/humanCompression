d3da8f2b50eeb7b48c71098e11594f83e3b6ad87
kellyyyang <kelyang@hmc.edu>
2022-11-17 22:19:00 -0800

    finished isHuman function

--- compress.py
+++ compress.py
@@ -16,15 +16,15 @@ def colorQuantizeImg(img, numColors):
 def sampleImg(img):
     pass
 
-def isHuman(img, orig_coordinates):
+def isHuman(img, orig_coords):
     ''' inputs:     img, pathname of file
                     orig_coordinates, a 4-tuple of the top-left and bottom-right coords of the face
         outputs:    True, if the new coordinates are close to orig_coordinates
                     False otherwise
     '''
     new_coords = haar_cascade(img)
-    width, height = new_coords[2] - new_coords[0], new_coords[1] - new_coords[3]
-    # if new_coords
+    width, height = abs(new_coords[2] - new_coords[0]), abs(new_coords[1] - new_coords[3])
+    return (orig_coords[0] - width*0.1 <= new_coords[0] <= orig_coords[0] + width*0.1) and (orig_coords[1] - height*0.1 <= new_coords[1] <= orig_coords[1] + height*0.1) and (orig_coords[2] - width*0.1 <= new_coords[2] <= orig_coords[2] + width*0.1) and (orig_coords[3] - height*0.1 <= new_coords[3] <= orig_coords[3] + height*0.1)
 
 def haar_cascade(img):
     ''' inputs:     img, the pathname of the img
@@ -39,6 +39,14 @@ def haar_cascade(img):
         result = faces[0]
         x, y, w, h = result
         x1, y1 = x + w, y + h
+
+        # testing
+        # cv2.rectangle(img, (x, y), (x1, y1), (0, 0, 255), 2)
+        # cv2.imshow('image', img)
+  
+        # cv2.waitKey(0)
+        # cv2.destroyAllWindows()
+
         # cv2.rectangle(img, (x, y), (x1, y1), (0, 0, 255), 2)
         return (x, y, x1, y1) # top-left-x, top-left-y, bottom-right-x, bottom-right-y
     return (0, 0, 0, 0)
@@ -69,9 +77,9 @@ def main(portraits, option = "color_quantization"):
                 print(f"not a human!!")
                 break
 
-if __name__ == "__main__":
-    # get array of portrait file names 
-    path = "./photos"
-    portraits = os.listdir(path)
-    print(portraits)
-    main(portraits)
+# if __name__ == "__main__":
+#     # get array of portrait file names 
+#     path = "./photos"
+#     portraits = os.listdir(path)
+#     print(portraits)
+#     main(portraits)
