5c09658e7155d1c6c12d9011a12d88b1afd2c625
Lilly Lee <lillee@pop-os.localdomain>
2022-11-21 11:54:31 -0800

    added sampling function

--- compress.py
+++ compress.py
@@ -11,10 +11,18 @@ def colorQuantizeImg(img, numColors):
     pilImg = Image.open("./photos/" + img) 
     compImg = pilImg.quantize(colors=numColors, method = None, kmeans = 0,
     palette = None).convert('RGB')
+    pilImg.close()
     return compImg
 
-def sampleImg(img):
-    pass
+def sampleImg(img, width, height):
+    """ input: img is a string representing a filename to a portrait
+               width, height are ints that represent new img size
+        output: PIL img of newly sampled portrait
+    """
+    pilImg = Image.open("./photos/" + img)
+    finalImg = piImg.resize((width, height),Image.ANTIALIAS)
+    pilImg.close()
+    return finalImg
 
 def isHuman(img, orig_coords):
     ''' inputs:     img, pathname of file
@@ -30,7 +38,9 @@ def haar_cascade(img):
     ''' inputs:     img, the pathname of the img
         outputs:    a 4-tuple containing the top-left and bottom-right coordinates of the face
     '''
-    classifier = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+    classifier = cv2.CascadeClassifier(cv2.data.haarcascades +
+    'haarcascade_frontalface_default.xml')
+
     img = cv2.imread(img)
     faces = classifier.detectMultiScale(img) # result
     # to draw faces on image
@@ -53,24 +63,38 @@ def haar_cascade(img):
 
 def main(portraits, option = "color_quantization"):
     # loop through each portrait
-    for portrait in portraits:
-        flag = False
-        
+    for portrait in portraits:        
         # run haar_cascade on orig file
-        orig_coordinates = haar_cascade(portrait)
 
         # make a copy of the file
-        shutil.copyfile(portrait, "compressed_" + portrait)
-        portrait = "compressed_" + portrait
+        shutil.copyfile("./photos/" + portrait, "./compress_photos/" + portrait)
         numColors = 255
-        while flag and numColors > 25: 
+        portraitPil = Image.open("./photos/" + portrait)
+        width, height = portraitPil.size
+        percent = 1
+        portraitPil.close()
+        portrait = "./compress_photos/" + portrait
+
+        orig_coordinates = haar_cascade(portrait)
+        
+        while True: 
             # check if portrait is identifiable as human
+
             if isHuman(portrait, orig_coordinates):
+                print(portrait)
                 if option == "color_quantization":
-                    newImg = colorQuantizeImg(portrait, numColors - 25)
-                    newImg.save("./photos/" + portrait)
+                    if numColors > 25:
+                        break
+                    numColors -= 25
+                    newImg = colorQuantizeImg(portrait, numColors)
+                    newImg.save("./compress_photos/" + portrait)
                 elif option == "sampling":
-                    pass 
+                    if percent <= 0:
+                        break
+                    newImg = sampleImg(portrait, width * percent, height *
+        percent)
+                    newImg.save("./compress_photos/" + portrait)
+                    percent -= 0.1
                 else:
                     print("invalid option")
             else:
@@ -79,7 +103,7 @@ def main(portraits, option = "color_quantization"):
 
 if __name__ == "__main__":
     # get array of portrait file names 
-    path = "test_photos"
+    path = "./photos/"
     portraits = os.listdir(path)
     print(portraits)
     main(portraits)
